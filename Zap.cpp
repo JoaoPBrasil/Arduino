ad6cef0c79f1bc0c4e88aaec308131df9cb3e07e
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2023-10-17 20:20:16 -0300

    Update Zap.cpp

--- Zap.cpp
+++ Zap.cpp
@@ -10,3 +10,20 @@ void loop()
   float valor = analogRead(A0);
   Serial.println((valor * 5) / 1023);
 }
+
+//ou
+
+// C++ code
+//
+void setup()
+{
+ Serial.begin(9600);
+}
+
+void loop()
+{
+  int valor = analogRead(A0);
+  float volts = (valor * 5.0f / 1023.0f);
+  Serial.print(volts);
+  Serial.println("V");
+}
